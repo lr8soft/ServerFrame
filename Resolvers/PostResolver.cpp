f4fcc75e7a51975d0dc58639247e7652c2b792c5
lrsoft <lrsoft@hotmail.com>
2023-04-16 17:33:23 +0800

    v0.134 rapidjson

--- Resolvers/PostResolver.cpp
+++ Resolvers/PostResolver.cpp
@@ -4,6 +4,8 @@
 
 #include <iostream>
 #include <sstream>
+#include <rapidjson/document.h>
+#include <rapidjson/writer.h>
 #include "PostResolver.h"
 #include "../Core/Request.hpp"
 #include "../Core/Reply.h"
@@ -25,6 +27,21 @@ bool PostResolver::handleRequest(const Request &req, Reply &rep) {
         stream << item.first << ":" << item.second << " len: " << item.second.length() << std::endl;
     }
 
-    Reply::setReply(rep, stream.str(), "html");
+    rapidjson::StringBuffer buffer;
+    rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+
+    writer.StartObject();
+    writer.Key("status");
+    writer.String("operation_success");
+
+    writer.Key("result");
+    writer.String(stream.str().c_str());
+
+    writer.EndObject();
+
+    std::cout << buffer.GetString() << std::endl;
+
+
+    Reply::setReply(rep, buffer.GetString(), "json");
     return true;
 }
