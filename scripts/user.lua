7caf755e7c02538d858d67307337908875026c86
lrsoft <lrsoft@hotmail.com>
2023-05-10 10:02:10 +0800

    v0.152 demo update

--- scripts/user.lua
+++ scripts/user.lua
@@ -6,9 +6,37 @@ user = {}
 
 user.login = function(request)
     response = JsonResponse:New()
+    -- 检测是否post
+    if request.METHOD ~= "POST" then
+        response:setContent({ status = "operation_failed", info = "请求方法错误" })
+        return response
+    end
+    -- 请求完整检测
+    local username = request.POST["username"]
+    local password = request.POST["password"]
+    if username == nil or password == nil then
+        response:setContent({ status = "operation_failed", info = "请求参数错误" })
+        return response
+    end
+    print("username:", username, "password:", password)
+    -- 找一下用户
+    local user = DB.User.get
+           :where({username = username})
+           :first()
+    -- 检测用户是否存在
+    if user == nil then
+        response:setContent({ status = "operation_failed", info = "用户不存在" })
+        return response
+    end
+    -- 检测密码是否正确
+    if user.password ~= password then
+        response:setContent({ status = "operation_failed", info = "密码错误" })
+        return response
+    end
+    -- 生成token
     result = { 
         status = "operation_success",
-        info = "哎呀你干嘛哎呀你干嘛哎呀你干嘛哎呀你干嘛"
+        info = "登录成功"
     }
 
     strResult = token.GetToken(result)
