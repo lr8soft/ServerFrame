d42228597a268899b7ef25b48d2c981a0ccd9492
lrsoft <lrsoft@hotmail.com>
2023-05-10 10:38:46 +0800

    v0.152 demo regist update

--- scripts/user.lua
+++ scripts/user.lua
@@ -18,7 +18,7 @@ user.login = function(request)
         response:setContent({ status = "operation_failed", info = "请求参数错误" })
         return response
     end
-    print("username:", username, "password:", password)
+    print("user login username:", username)
     -- 找一下用户
     local user = DB.User.get
            :where({username = username})
@@ -40,27 +40,47 @@ user.login = function(request)
     }
 
     strResult = token.GetToken(result)
-    print("jwt_token:", strResult)
     response:setHeader("jwt_token", strResult)
     response:setContent(result)
     return response
 end
 
 user.regist = function(request)
+    response = JsonResponse:New()
+    -- 请求完整检测
+    local username = request.POST["username"]
+    local password = request.POST["password"]
+    if username == nil or password == nil then
+        response:setContent({ status = "operation_failed", info = "请求参数错误" })
+        return response
+    end
+    print("user try regist username:", username)
+
+    local user = DB.User.get
+               :where({username = username})
+               :first()
+    -- 检测用户是否存在
+    if user ~= nil then
+        response:setContent({ status = "operation_failed", info = "用户已存在" })
+        return response
+    end
+    -- 创建用户
     local obj = DB.User({
-        username = "lrsoft",
-        password = "test",
+        username = username,
+        password = password,
         time_create = os.time()
     })
     obj:save()
-    print("REGIST!")
 
-    response = JsonResponse:New()
-    response:setContent({ status = "operation_success", request = request,
-                          user = {
-                              name = obj.username,
-                              id = obj.id
-                          } })
+    result = {
+        status = "operation_success",
+        info = "注册成功",
+        user = {
+            name = obj.username,
+            id = obj.id
+        }
+    }
+    response:setContent(result)
     return response
 end
 
